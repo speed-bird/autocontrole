ddafcac8867de32f73a3cd5df2cda3bf4d080b9a
Jonathan Robert <jr@MacBook-Pro-de-JR.local>
2024-11-21 22:51:05 +0100

    Deploy new build

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -14,7 +14,7 @@ const LoginForm = () => {
 
     try {
       // Envoi de la requête au backend
-      const response = await fetch('http://localhost:3001/fetch-reservations', {
+      const response = await fetch('http://autocontrole-backend-production.up.railway.app/fetch-reservations', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
